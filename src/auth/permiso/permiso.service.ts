904925e62272ca05b89a6608061031cc75a67c88
areyesarean <alereyarean@gmail.com>
2021-11-19 22:22:41 +0100

    create crud Permiso entity in permiso.service

--- src/auth/permiso/permiso.service.ts
+++ src/auth/permiso/permiso.service.ts
@@ -1,26 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Observable } from 'rxjs';
+import { Repository } from 'typeorm';
 import { CreatePermisoInput } from './dto/create-permiso.input';
 import { UpdatePermisoInput } from './dto/update-permiso.input';
+import { Permiso } from './entities/permiso.entity';
 
 @Injectable()
 export class PermisoService {
-  create(createPermisoInput: CreatePermisoInput) {
-    return 'This action adds a new permiso';
+  constructor(
+    @InjectRepository(Permiso)
+    private readonly _permisoRepo: Repository<Permiso>
+  ) {}
+
+  async create(createPermisoInput: CreatePermisoInput): Promise<Permiso> {
+    const newPermiso = this._permisoRepo.create({ ...createPermisoInput });
+    return await this._permisoRepo.save(newPermiso);
   }
 
-  findAll() {
-    return `This action returns all permiso`;
+  async findAll(): Promise<Permiso[]> {
+    const permisos = await this._permisoRepo.find();
+    if (!permisos) throw new NotFoundException('No existen Permisos');
+    return permisos;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} permiso`;
+  async findOne(id: string): Promise<Permiso> {
+    const permiso = await this._permisoRepo.findOne(id);
+    if (!permiso) throw new NotFoundException(`No existe el permiso`);
+    return permiso;
   }
 
-  update(id: number, updatePermisoInput: UpdatePermisoInput) {
-    return `This action updates a #${id} permiso`;
+  async update(id: string, updatePermisoInput: UpdatePermisoInput) {
+    const permiso = await this._permisoRepo.findOne(id);
+    if (!permiso) throw new NotFoundException(`No existe el permiso`);
+    const permisoUpdate = this._permisoRepo.merge(permiso, updatePermisoInput);
+    return await this._permisoRepo.save(permisoUpdate);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} permiso`;
+  async remove(id: string): Promise<any> {
+    const permiso = await this._permisoRepo.findOne(id);
+    if (!permiso) throw new NotFoundException(`No existe el permiso`);
+    await this._permisoRepo.delete(id);
+    return permiso;
   }
 }
