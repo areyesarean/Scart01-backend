0622093d23842e5f89cae23e9b5c47820f3292cf
areyesarean <alereyarean@gmail.com>
2021-11-19 19:10:52 +0100

    enabled configuration graphql and typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmConfig } from './database/typeorm.config';
+import * as path from 'path';
+import { graphqlProviders } from './graphql/graphql.provider';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: path.resolve(__dirname, `../env/development.env`),
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync(TypeOrmConfig),
+    //GraphQLModule.forRootAsync(graphqlProviders),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
