4d151a7248b5623b532d12f81fccea2c63950892
areyesarean <alereyarean@gmail.com>
2021-11-19 22:19:02 +0100

    add decorator @IsNotEmpty to permisoName

--- src/auth/permiso/dto/create-permiso.input.ts
+++ src/auth/permiso/dto/create-permiso.input.ts
@@ -1,9 +1,10 @@
 import { InputType, Field } from '@nestjs/graphql';
-import { IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 @InputType()
 export class CreatePermisoInput {
   @IsString()
+  @IsNotEmpty()
   @Field(() => String, { description: 'Nombre del permiso' })
   permisoName: string;
 }
