329822e1e51c1acdbd9e17b9bfe011db21f84186
areyesarean <alereyarean@gmail.com>
2021-11-19 22:20:55 +0100

    create entity Permiso

--- src/auth/permiso/entities/permiso.entity.ts
+++ src/auth/permiso/entities/permiso.entity.ts
@@ -1,7 +1,14 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, Int, ID } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity()
 @ObjectType()
 export class Permiso {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => ID, { description: 'id del permiso en formato uuid' })
+  id: string;
+
+  @Column({ unique: true })
+  @Field(() => String, { description: 'Nombre del permiso' })
+  permisoName: string;
 }
