ac8ae04380520f1af59bc975b7e1863ea1c0c5fa
areyesarean <alereyarean@gmail.com>
2021-11-19 22:20:17 +0100

    add entity permiso to typeorm.forFeature()

--- src/auth/permiso/permiso.module.ts
+++ src/auth/permiso/permiso.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PermisoService } from './permiso.service';
 import { PermisoResolver } from './permiso.resolver';
+import { Permiso } from './entities/permiso.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  providers: [PermisoResolver, PermisoService]
+  imports: [TypeOrmModule.forFeature([Permiso])],
+  providers: [PermisoResolver, PermisoService],
 })
 export class PermisoModule {}
