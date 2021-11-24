42d10afa928a60d34f257c5e11a1002facd43a08
areyesarean <alereyarean@gmail.com>
2021-11-24 23:08:35 +0100

    add Rol and Permiso Entitys in typerorm.forFeature

--- src/auth/permiso/permiso.module.ts
+++ src/auth/permiso/permiso.module.ts
@@ -3,9 +3,10 @@ import { PermisoService } from './permiso.service';
 import { PermisoResolver } from './permiso.resolver';
 import { Permiso } from './entities/permiso.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Rol } from '../rol/entities/rol.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Permiso])],
+  imports: [TypeOrmModule.forFeature([Permiso, Rol])],
   providers: [PermisoResolver, PermisoService],
 })
 export class PermisoModule {}
