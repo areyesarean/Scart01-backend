a373f09acbc10f332904be4d0b781f43bdcd7ae5
areyesarean <alereyarean@gmail.com>
2021-11-24 23:07:24 +0100

    add RolModule to imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PermisoModule } from './permiso/permiso.module';
+import { RolModule } from './rol/rol.module';
 
 @Module({
-  imports: [PermisoModule],
+  imports: [PermisoModule, RolModule],
 })
 export class AuthModule {}
