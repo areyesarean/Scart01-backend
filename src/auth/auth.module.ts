1bef29788d5a97bb0c100e87f7b994866406552d
areyesarean <alereyarean@gmail.com>
2021-11-27 13:49:53 +0100

    create resource user

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PermisoModule } from './permiso/permiso.module';
 import { RolModule } from './rol/rol.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PermisoModule, RolModule],
+  imports: [PermisoModule, RolModule, UserModule],
 })
 export class AuthModule {}
