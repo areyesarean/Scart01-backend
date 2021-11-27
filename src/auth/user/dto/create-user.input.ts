4dfe0a44282d1687d0d87c0879d6601df9a13dd1
areyesarean <alereyarean@gmail.com>
2021-11-27 14:06:00 +0100

    create userDto

--- src/auth/user/dto/create-user.input.ts
+++ src/auth/user/dto/create-user.input.ts
@@ -1,7 +1,46 @@
-import { InputType, Int, Field } from '@nestjs/graphql';
+import { InputType, Field } from '@nestjs/graphql';
+import {
+  IsBoolean,
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Length,
+} from 'class-validator';
 
 @InputType()
 export class CreateUserInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @IsString()
+  @IsNotEmpty()
+  @Field(() => String, { description: 'Nombre de usuario' })
+  username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(8, 16)
+  @Field(() => String, { description: 'Contrasenna' })
+  password: string;
+
+  @IsString()
+  @Field(() => String, { description: 'Nombre del usuario' })
+  firstName: string;
+
+  @IsString()
+  @Field(() => String, { description: 'Apellidos del usuario' })
+  lastName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  @Field(() => String, { description: 'Correo del usuario' })
+  email: string;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  @Field(() => Boolean, { description: 'Es Activo ?' })
+  active: boolean;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  @Field(() => Boolean, { description: 'Es verificado ?' })
+  verified: boolean;
 }
