d30aae14170523b369245fa4e089e1ba134d6ca5
areyesarean <alereyarean@gmail.com>
2021-11-30 11:16:44 +0100

    create user Interface

---
+++ src/auth/user/interfaces/user.interface.ts
@@ -0,0 +1,10 @@
+export interface UserInterface {
+  id: string;
+  username: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  active: boolean;
+  verified: boolean;
+  rol: any;
+}
