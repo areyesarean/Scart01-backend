2e38e6954a1acb8f88084b4a399e809dd1f6c00a
areyesarean <alereyarean@gmail.com>
2021-11-30 11:20:13 +0100

    imports intities Rol and User in typeorm.forfeature

--- src/auth/user/user.module.ts
+++ src/auth/user/user.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserResolver } from './user.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Rol } from '../rol/entities/rol.entity';
 
 @Module({
-  providers: [UserResolver, UserService]
+  imports: [TypeOrmModule.forFeature([User, Rol])],
+  providers: [UserResolver, UserService],
 })
 export class UserModule {}
