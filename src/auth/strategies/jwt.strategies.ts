35a6073fee1b4ee2e8a672344ba641b0bc1e6b2c
areyesarean <alereyarean@gmail.com>
2021-12-02 10:55:29 +0100

    validate method in jwt strategy

--- src/auth/strategies/jwt.strategies.ts
+++ src/auth/strategies/jwt.strategies.ts
@@ -2,10 +2,17 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../user/entities/user.entity';
+import { Repository } from 'typeorm';
+import { PayloadInterface } from '../login/interface/payload.interface';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private _configService: ConfigService) {
+  constructor(
+    private _configService: ConfigService,
+    @InjectRepository(User) private readonly _userRepo: Repository<User>
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -13,9 +20,10 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: any) {
-    console.log(payload.username);
-    
-    return { username: payload.username };
+  async validate({ iat, exp, ...payload }: PayloadInterface) {
+    const user = await this._userRepo.findOne({
+      where: { ...payload },
+    });
+    return !user ? false : true;
   }
 }
